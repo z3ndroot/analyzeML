716e884511eea2eac7b1c9ae320737dd7c90fb09
z3ndroot <jonhrip9@gmail.com>
2023-02-19 12:43:36 +0300

    refined the class ExcelWriter

--- pyxlutils/excel_writer.py
+++ pyxlutils/excel_writer.py
@@ -1,5 +1,18 @@
-
+from openpyxl import Workbook
 
 class ExcelWriter:
-    pass
-    #TODO....
+    def __init__(self, filename):
+        self.filename = filename
+
+    def write_data(self, data):
+        wb = Workbook()
+        sheet = wb.active
+
+        sheet.cell(row=1, column=1, value="Message")
+        sheet.cell(row=1, column=2, value="Result")
+
+        for row_idx, row_data in enumerate(data, start=2):
+            sheet.cell(row=row_idx, column=1, value=row_data[0])
+            sheet.cell(row=row_idx, column=2, value=row_data[1])
+
+        wb.save(self.filename)
