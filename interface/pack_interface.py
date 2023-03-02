7f35fca90c3711333e57d7828958dd2ef0a6b8d8
z3ndroot <jonhrip9@gmail.com>
2023-03-02 15:00:06 +0300

    add error messagebox
    new logic check predicate

--- interface/pack_interface.py
+++ interface/pack_interface.py
@@ -7,7 +7,9 @@ from pyxlutils import (
     ExcelWriter,
     analyze_messages_with_rules,
     analyze_meta_with_rules,
+    check_predicate,
     JSONMetaData,
+
 )
 
 
@@ -17,11 +19,9 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.open_xlsx)
         self.pushButton_2.clicked.connect(self.open_rules)
-        self.pushButton_3.clicked.connect(self.path_to_file)
         self.comboBox.currentTextChanged.connect(self.update_columns)
         self.pushButton_4.clicked.connect(self.analyze)
         self.__file = None
-        self.__data = None
 
     def open_xlsx(self):
         self.comboBox.clear()
@@ -38,7 +38,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.__file = JSONMetaData(file)
             self.comboBox_2.clear()
 
-
     def update_columns(self):
         if self.comboBox.currentText():
             self.comboBox_2.clear()
@@ -49,29 +48,51 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         rules_file, _ = QtWidgets.QFileDialog.getOpenFileName(self, filter="*.txt")
         self.lineEdit_2.setText(rules_file)
 
-    def path_to_file(self):
-        path = QtWidgets.QFileDialog.getExistingDirectory(self)
-        self.lineEdit_3.setText(path)
-
     def analyze(self):
-        path_save = self.lineEdit_3.text()
         path_rules = self.lineEdit_2.text()
 
-        if not (check_path(path_rules) and check_path(path_save)):
+        if not check_path(path_rules):
+            self.MessageBox_2.setWindowTitle("PathRules")
+            self.MessageBox_2.setText("The rules file does not exist or the path is wrong")
+            self.MessageBox_2.exec_()
             return
+
+        error = check_predicate(path_rules)
+
+        if error:
+            self.MessageBox_2.setWindowTitle("PredicateError")
+            self.MessageBox_2.setText(error)
+            self.MessageBox_2.exec_()
+            return
+
+        if not self.__file:
+            self.MessageBox_2.setWindowTitle("FileOpen")
+            self.MessageBox_2.setText("The message file is not selected or the path is wrong")
+            self.MessageBox_2.exec_()
+            return
+
+        path_save, _ = QtWidgets.QFileDialog.getSaveFileName(self, filter="*.xlsx")
+
+        if not path_save:
+            self.MessageBox_2.setWindowTitle("SaveFile")
+            self.MessageBox_2.setText("File to save is not selected")
+            self.MessageBox_2.exec_()
+            return
+
         if isinstance(self.__file, ExcelReader):
             messages = self.__file.get_messages(
                 sheet_name=self.comboBox.currentText(),
                 column_name=self.comboBox_2.currentText()
             )
             data = analyze_messages_with_rules(messages, path_rules)
-            ew = ExcelWriter(path_save + "/result_excel.xlsx", name_column=["Message", "Result"])
+
+            ew = ExcelWriter(path_save, name_column=["Message", "Result"])
             ew.write_data(data)
 
         elif isinstance(self.__file, JSONMetaData):
             data = analyze_meta_with_rules(self.__file.meta_info, path_rules)
-            ew = ExcelWriter(path_save + "/result_json.xlsx", name_column=["Id","Message", "Result"])
+
+            ew = ExcelWriter(path_save, name_column=["Id", "Message", "Result"])
             ew.write_data(data)
-
-
+        self.MessageBox.setText(f"{path_save} file saved✅")
         self.MessageBox.exec_()
